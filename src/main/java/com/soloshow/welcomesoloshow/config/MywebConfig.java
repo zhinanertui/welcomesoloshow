0eb068b1b101b2806fb55f01d662f542db46ebfc
xingshiwei <login_ik@163.com>
2019-05-30 09:07:38 +0800

    测试页面

--- src/main/java/com/soloshow/welcomesoloshow/config/MywebConfig.java
+++ src/main/java/com/soloshow/welcomesoloshow/config/MywebConfig.java
@@ -1,8 +1,11 @@
 package com.soloshow.welcomesoloshow.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.io.File;
 
 /**
  * @Author xsw
@@ -13,5 +16,10 @@ public class MywebConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("/hello");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/video/**").addResourceLocations("file:" + File.separator +"D:" + File.separator + "videos" + File.separator);
     }
 }
--- src/main/java/com/soloshow/welcomesoloshow/controller/IndexController.java
+++ src/main/java/com/soloshow/welcomesoloshow/controller/IndexController.java
@@ -2,16 +2,72 @@ package com.soloshow.welcomesoloshow.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.annotation.MultipartConfig;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 /**
  * @author  xsw
  * @since  2018/11/29
  */
 @Controller
+@MultipartConfig
 public class IndexController {
     @RequestMapping("/index")
     public String index(ModelMap map){
         return "index";
     }
+
+    @RequestMapping("/video")
+    public String videoHtml() {
+        return "video";
+    }
+
+    @RequestMapping("/uploadHtml")
+    public String uploadHtml() {
+        return "upload";
+    }
+
+    @RequestMapping("/test")
+    public String test() {
+        return "test";
+    }
+    @PostMapping("/uploadFile")
+    @ResponseBody
+    public Map<String,Integer> uploadFile(MultipartFile file,String ids) {
+        String fileName = file.getOriginalFilename();
+        String filePath = "D:/videos/";
+        // 提取文件拓展名\
+        String fileNameExtension = "";
+        if(fileName!=null){
+            fileNameExtension= fileName.substring(fileName.indexOf("."));
+        }
+        //重命名
+        String realName = UUID.randomUUID().toString() + System.currentTimeMillis() + fileNameExtension;
+        File file1 = new File(filePath + realName);
+        Map<String, Integer> map = new HashMap<>(500);
+        System.out.println();
+        try {
+            file.transferTo(file1);
+        } catch (IOException e) {
+            e.printStackTrace();
+            map.put("code", 500);
+            return map;
+        }
+        map.put("code", 200);
+        return map;
+    }
+
+    @GetMapping("/hellos")
+    @ResponseBody
+    public String hello() {
+        return "测试";
+    }
 }
+
